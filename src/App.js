b10bf0e7eef4dc9aba38177252687911980241e7
chenjunCodingGit <271676725@qq.com>
2021-01-12 22:51:29 +0800

    定时刷新

--- src/App.js
+++ src/App.js
@@ -1,54 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-
 function App() {
-
-  function formatName(user){
-    // return <h1>Hello, Stranger.</h1> + user;
-    return user.name + ' ' + user.lastname;
-  }
-
-  const user = {
-    name: 'kangkang',
-    lastname: 'jane'
-  }
-
-  const element = (
-    <h1 className="greeting">
-      Hello, world!333333333333333
-    </h1>
-  );
-
-    const element2 = React.createElement(
-      'h1',
-      {className: 'greeting'},
-      '2222222222222'
-    )
-
-    const element3 = <h1>ddddd33ddddddd</h1>;
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {element}
-        {element2}
-        {element3}
-        <div>hello {formatName(user)}</div>
-      </header>
+      <h1>Hello, world!</h1>
+      <h2>it is {new Date().toLocaleTimeString()}</h2>
     </div>
   );
 }

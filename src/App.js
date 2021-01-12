0ac55872b5347903337eb17d08be3f57eca98da6
chenjunCodingGit <271676725@qq.com>
2021-01-13 00:05:59 +0800

    分离组件

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@
 import './App.css';
 import React from 'react';
 
+import Comment from '../src/test/test'
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'https://placekitten.com/g/64/64',
+  }
+};
+
 function Welcome(props) {
   return <h1>她的名字是：{props.name}</h1>
 }
@@ -21,6 +32,12 @@ function App() {
       <Welcome name="jane2" />
       <Welcome2 name='maria'/>
       <Welcome2 name='maria2'/>
+
+      <Comment 
+        author= {comment.author}
+        text= {comment.text}
+        date= {comment.date}
+      />
     </div>
   );
 }

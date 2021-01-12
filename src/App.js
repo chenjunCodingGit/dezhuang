be2214f0e1471903ce274e3da4d00f177ff22672
chenjun <chenjun@chenjundeMacBook-Pro.local>
2021-01-12 21:34:53 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
 
 function App() {
+
+  function formatName(user){
+    // return <h1>Hello, Stranger.</h1> + user;
+    return user.name + ' ' + user.lastname;
+  }
+
+  const user = {
+    name: 'kangkang',
+    lastname: 'jane'
+  }
+
+  const element = (
+    <h1 className="greeting">
+      Hello, world!333333333333333
+    </h1>
+  );
+
+    const element2 = React.createElement(
+      'h1',
+      {className: 'greeting'},
+      '2222222222222'
+    )
+
+    const element3 = <h1>ddddd33ddddddd</h1>;
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +44,10 @@ function App() {
         >
           Learn React
         </a>
+        {element}
+        {element2}
+        {element3}
+        <div>hello {formatName(user)}</div>
       </header>
     </div>
   );

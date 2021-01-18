3caccdd440796970d7669a95072c136db83ae481
chenjunCodingGit <271676725@qq.com>
2021-01-18 23:13:56 +0800

    antd

--- src/App.js
+++ src/App.js
@@ -1,40 +1,7 @@
-// import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-// import Comment from '../src/test/test'
 import Clock from '../src/test/Clock'
-import LoginControl from '../src/test/LoginControl'
-// import NumberList from '../src/test/NumberList'
-import NameForm from '../src/test/NameForm'
-import Reservation from '../src/test/Reservation'
-import Calculator from '../src/test/Calculator'
-import ThemedButton from '../src/test/ThemedButton'
-import ContextContextApp from '../src/test/ThemeContext/app'
-import GuestContext from '../src/test/ThemeContext/GuestContext'
-import NameRefsForm from '../src/test/NameRefsForm'
-import HookCounter from '../src/test/HookCounter'
-
-
-
-// const comment = {
-//   date: new Date(),
-//   text: 'I hope you enjoy learning React!',
-//   author: {
-//     name: 'Hello Kitty',
-//     avatarUrl: 'https://placekitten.com/g/64/64',
-//   }
-// };
-
-// function Welcome(props) {
-//   return <h1>她的名字是：{props.name}</h1>
-// }
-
-// class Welcome2 extends React.Component{
-//   render(){
-//     return <h1>欢迎第二个人：{this.props.name}</h1>
-//   }
-// }
 
 class Toggle extends React.Component {
   
@@ -43,9 +10,6 @@ class Toggle extends React.Component {
     this.state = {
       isBtnState: false
     }
-
-    // 为了在回调中使用 `this`，这个绑定是必不可少的
-    // this.onClickHandle = this.onClickHandle.bind(this)
   }
 
   onClickHandle = ()=> {
@@ -68,21 +32,6 @@ function App() {
     <div className="App">
       <Clock/>
       <Toggle></Toggle>
-      <LoginControl/>
-      {/* <NumberList numbers={[1,2,3,5,4,8]}/> */}
-      <NameForm/>
-      <Calculator/>
-      <Reservation/>
-      <ThemedButton/>
-      <ContextContextApp/>
-      <NameRefsForm/>
-      <HookCounter/>
-      <GuestContext signedInUser={'Guest1'} theme={'dark'}/>
-      {/* <Comment 
-        author= {comment.author}
-        text= {comment.text}
-        date= {comment.date}
-      /> */}
     </div>
   );
 }

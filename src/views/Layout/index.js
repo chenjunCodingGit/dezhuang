09bda935982cc1b9e5eb6d33ec3d991c78ced444
chenjunCodingGit <271676725@qq.com>
2021-01-25 14:15:44 +0800

    完成侧边栏

--- src/views/Layout/index.js
+++ src/views/Layout/index.js
@@ -16,43 +16,132 @@ import {
   FileTextOutlined
 } from '@ant-design/icons'
 
-
 import style from './index.module.scss'
-const { Header, Sider, Content } = Layout
 
-// const style = require('./index.module.scss')
+const { Header, Sider, Content } = Layout
+const { $http } = React
+const mapMenu = [
+  { key: 'home', name: '首页'},
+  { key: 'user', name: '用户列表', parentName: '用户管理', parentKey: 'user-menu' },
+  { key: 'roles', name: '角色管理', parentName: '用户管理', parentKey: 'user-menu' },
+  { key: 'articles', name: '文章列表', parentName: '文章管理', parentKey: 'article-menu' },
+  { key: 'category', name: '文章分类', parentName: '文章管理', parentKey: 'article-menu' },
+  { key: 'comments', name: '文章评论', parentName: '文章管理', parentKey: 'article-menu' },
+  { key: 'message', name: '消息中心', parentName: '社区管理', parentKey: 'community' },
+  { key: 'website-setting', name: '网站设置', parentName: '设置管理', parentKey: 'setting-menu' },
+  { key: 'email-service', name: '邮件服务', parentName: '设置管理', parentKey: 'setting-menu' },
+  { key: 'basic-info', name: '基本资料', parentName: '设置管理', parentKey: 'setting-menu' },
+  { key: 'modify-password', name: '修改密码', parentName: '设置管理', parentKey: 'setting-menu' },
+]
 
 
 class LayoutContainer extends React.Component {
   constructor(props) {
     super(props)
-    console.log('props: ', props)
+    const currentPath = this.props.location.pathname.substr(1)
+    const openKeys = this.handleFindOpenMenu(currentPath)
+    const userInfo = JSON.parse(sessionStorage.getItem('userInfo')) || {}
+
     this.state = {
       collapsed: false,
       title: '后台管理系统',
+      selectedKeys: [currentPath],
+      defaultOpenKeys: [openKeys.menuKey],
+      breadcrumb: openKeys.breadcrumb,
+      userInfo
+    }
+  }
 
+  handleFindOpenMenu = (selectedKeys) => {
+    const findMenu = mapMenu.find(subMenu => subMenu.key === selectedKeys)
+    if (selectedKeys === '404') {
+      this.props.history.push({
+        pathname: '/login'
+      })
+      return {
+        menuKey: findMenu && findMenu.parentKey,
+        subMenu: findMenu && findMenu.key,
+        breadcrumb: []
+      }
+    }
+
+    let breadcrumb = []
+    breadcrumb.push(findMenu.parentName, findMenu.name)
+    breadcrumb = breadcrumb.filter(v => v)
+    return {
+      menuKey: findMenu && findMenu.parentKey,
+      subMenu: findMenu && findMenu.key,
+      breadcrumb
+    }
+  }
+
+  handleRouter = (item, key) => {
+    const { history } = this.props
+    const findMenu = mapMenu.find(subMenu => subMenu.key === item.key)
+    let breadcrumb = []
+    breadcrumb.push(findMenu.parentName, findMenu.name)
+    breadcrumb = breadcrumb.filter(v => v)
+    this.setState({
+      selectedKeys: [item.key],
+      breadcrumb
+    }, () => {
+      history.push(item.key)
+    })
+  }
+
+  handleMenuClick = (item) => {
+    console.log('item: ', item)
+    const { history } = this.props
+    const { key } = item
+    if (key === 'logout') {
+      if (key === 'logout') {
+        $http.get('logout').then(() => {
+          sessionStorage.clear()
+          history.push('/login')
+        })
+      } else {
+        this.setState({
+          selectedKeys: [item.key]
+        }, () => {
+          history.push(item.key)
+        })
+      }
     }
   }
 
   toggle = () => {
     this.setState({
       collapsed: !this.state.collapsed,
+      title: this.state.collapsed ? '后台管理系统' : ''
     });
   };
 
   render() {
     const { route } = this.props
-    const { collapsed, title } = this.state
-    const breadcrumb = [1,2,3,4]
+    const { collapsed, title, selectedKeys, userInfo, defaultOpenKeys, breadcrumb } = this.state
+    
+    const userDropdownMenu = (
+      <Menu onClick={this.handleMenuClick}>
+        <Menu.Item key="basic-info">基本资料</Menu.Item>
+        <Menu.Item key="modify-password">修改密码</Menu.Item>
+        <Menu.Divider />
+        <Menu.Item key="logout">退出</Menu.Item>
+      </Menu>
+    )
+    
     return(
-      <HashRouter>
+      <HashRouter replace>
         <Layout className={style['layout-container']}>
           <Sider trigger={null} collapsible collapsed={collapsed}>
             <div className={style['logo']}>
               <CodepenCircleOutlined className={style['icon']}/>
               <span>{title}</span>
             </div>
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={['home']}>
+            <Menu theme="dark" mode="inline"
+            defaultSelectedKeys={selectedKeys}
+            defaultOpenKeys={defaultOpenKeys}
+            onClick={this.handleRouter}
+            >
               <Menu.Item key="home" icon={<HomeOutlined />}>
                 首页
               </Menu.Item>
@@ -117,6 +206,17 @@ class LayoutContainer extends React.Component {
                 className: style['trigger'],
                 onClick: this.toggle,
               })}
+            
+            <div className={style['header-right']}>
+              <div className="info mr-20">
+                <Avatar src={userInfo.avatar} />
+                <Dropdown overlay={userDropdownMenu} trigger={['hover']}>
+                <Button type="link" className={style['btn-user']}>
+                        {userInfo.username}<DownOutlined />
+                      </Button>
+              </Dropdown>
+              </div>
+            </div>
             </Header>
             <Content className={style['layout-content']}>
               <Breadcrumb className={style['layout-nav']}>
